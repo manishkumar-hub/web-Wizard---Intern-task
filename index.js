9f234931c0484d8879cadbda48c0fcd0884f00b5
manish <manishkumarpandit12@gmail.com>
2022-06-13 19:04:08 +0530

    Code improvement

--- index.js
+++ index.js
@@ -22,47 +22,68 @@ const addImage = (e) => {
 
 const imgInp = document.getElementById('upload-img')
 imgInp.addEventListener('change', addImage);
-
+var CanvaImage;
 reader.addEventListener("load", () => {
-    // console.log(reader.result);
-    fabric.Image.fromURL(reader.result, img => {
+    CanvaImage = fabric.Image.fromURL(reader.result, img => {
+        img.scale(0.6)
         canvas.add(img);
+        canvas.viewportCenterObjectV(img)
+        canvas.viewportCenterObjectH(img)
         canvas.requestRenderAll();
     })
 })
 
 canvas.on('mouse:move', (event) => {
+
     if (mouseClick) {
         const mEvent = event.e;
-        // console.log(mEvent)
         const delta = new fabric.Point(mEvent.movementX, mEvent.movementY);
         canvas.relativePan(delta)
     }
 })
 
 
+var mPosX, mPosY;
 canvas.on('mouse:wheel', function (opt) {
     var delta = opt.e.deltaY;
+    console.log(opt.e.clientX)
     var zoom = canvas.getZoom();
     zoom *= 0.999 ** delta;
-    // console.log(canvas.getZoom())
     if (zoom > 20) zoom = 20;
-    if (zoom < 0.01) zoom = 0.01;
+    if (zoom < 1) {
+        zoom = 1;
+        var objects = canvas.getObjects();
+        canvas.viewportCenterObjectV(objects[0])
+        canvas.viewportCenterObjectH(objects[0])
+    }
     if (delta < 0) {
+
+        mPosX = opt.e.clientX;
+        mPosY = opt.e.clientY;
+        console.log("while zoomin ", mPosX, mPosY);  
         canvas.zoomToPoint({ x: opt.e.offsetX, y: opt.e.offsetY }, zoom);
     }
     else {
-        const delta = new fabric.Point(0, 0);
-        canvas.zoomToPoint({ x: opt.e.offsetX, y: opt.e.offsetY }, zoom);
-        // canvas.setZoom(zoom);
-        canvas.absolutePan(delta)
+
+        if (mPosX == opt.e.clientX && mPosY == opt.e.clientY) {
+            console.log('equ')
+            canvas.zoomToPoint({ x: opt.e.offsetX, y: opt.e.offsetY }, zoom);
+        }
+        else {
+            var objects = canvas.getObjects();
+            canvas.viewportCenterObjectV(objects[0])
+            canvas.viewportCenterObjectH(objects[0])
+            const delta = new fabric.Point(40, 40);
+            canvas.zoomToPoint({ x: mPosX, y: mPosY }, zoom);
+            canvas.absolutePan(delta)
+        }
     }
     opt.e.preventDefault();
     opt.e.stopPropagation();
 });
 
 
-const reset= ()=>{
+const reset = () => {
     const delta = new fabric.Point(0, 0)
     canvas.absolutePan(delta);
     canvas.setZoom(1);
